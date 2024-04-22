7d8feb4f6e6456da0f70095601ef42c44cee1e7a
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-04-22 13:07:31 +0200

    created users res

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,9 @@
+export class User {
+
+    User_ID: number;
+    DNI: string;
+    Username: string;
+    Name: string;
+    Surname: string;
+    Password: string;
+}
