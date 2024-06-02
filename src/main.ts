087f666e2e52d102c40c4841f802838a363caca2
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-06-02 14:18:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('C:/Users/kills/Documents/_.myeventz.es_private_key.key'),
+    cert: fs.readFileSync('C:/Users/kills/Documents/myeventz.es_combined.cer')
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors({
-    origin: true, 
+    origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204
   });
-  
+
   await app.listen(3000);
 }
 bootstrap();
